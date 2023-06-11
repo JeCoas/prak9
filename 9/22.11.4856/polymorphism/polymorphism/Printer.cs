723e363dfda91ffedc3def47efae5270bd3a374e
JeCoas <127827888+JeCoas@users.noreply.github.com>
2023-06-11 23:52:33 +0700

    Add files via upload

---
+++ 9/22.11.4856/polymorphism/polymorphism/Printer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    public class Printer
+    {
+        public string namaprinter { get; set; }
+        public virtual void Merek()
+        {
+            Console.WriteLine("Merek Printer: ");
+        }
+    }
+
+    public class Epson : Printer
+    {
+        public override void Merek()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Epson display dimension : 10 * 11");
+            Console.WriteLine("Epson Printer Printing....");
+        }
+    }
+
+    public class Cannon : Printer
+    {
+        public override void Merek()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Cannon display dimension : 9.5 * 12");
+            Console.WriteLine("Cannon Printer Printing....");
+        }
+    }
+
+    public class LaserJett : Printer
+    {
+        public override void Merek()
+        {
+            Console.WriteLine();
+            Console.WriteLine("LaserJett display dimension 12 * 12");
+            Console.WriteLine("LaserJett Printer Printing....");
+        }
+    }
+}
+
+    
+
---
+++ 9/22.11.4856/polymorphism/polymorphism/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace polymorphism
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer Printer = new Printer();
+            Console.WriteLine("Pilih printer: ");
+            Console.WriteLine("1. Epson");
+            Console.WriteLine("2. Cannon");
+            Console.WriteLine("3. LaserJett");
+            Console.WriteLine();
+            Console.WriteLine("Nomor printer [1/2/3]: ");
+
+            Printer Epson = new Epson();
+            Printer Cannon = new Cannon();
+            Printer LaserJett = new LaserJett();
+
+            int nomorprinter = Convert.ToInt32(Console.ReadLine());
+
+            if (nomorprinter == 1)
+            {
+                Epson.Merek();
+            }
+            else if (nomorprinter == 2)
+            {
+                Cannon.Merek();
+            }
+            else if (nomorprinter == 3)
+            {
+                LaserJett.Merek();
+            }
+            else
+                Console.WriteLine("Nomor printer tidak valid!");
+
+            Console.ReadKey();
+        }
+    }
+}
